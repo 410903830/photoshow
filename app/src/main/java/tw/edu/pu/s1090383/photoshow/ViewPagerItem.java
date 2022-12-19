d220000c9c7124e13d4f50fa09011a5189c64643
410903830 <s1090383@gm.pu.edu.tw>
2022-12-19 12:00:01 +0800

    Initial commit

---
+++ app/src/main/java/tw/edu/pu/s1090383/photoshow/ViewPagerItem.java
@@ -0,0 +1,13 @@
+package tw.edu.pu.s1090383.photoshow;
+
+public class ViewPagerItem {
+
+    int imageID;
+    String heading, description;
+
+    public ViewPagerItem(int imageID, String heading, String description) {
+        this.imageID = imageID;
+        this.heading = heading;
+        this.description = description;
+    }
+}
